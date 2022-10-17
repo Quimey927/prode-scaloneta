fee8046565173b6a5438427970bd019f11220267
Quimey <quimeymata@gmail.com>
2022-10-17 13:34:17 -0300

    Fixes port error

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-const port = /* process.env.PORT ||  */3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
