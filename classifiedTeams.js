724cc25dcb0065c972690415e4a14f16c450536a
Quimey <quimeymata@gmail.com>
2022-12-01 18:06:46 -0300

    Actualices classifiedTeams file

--- classifiedTeams.js
+++ classifiedTeams.js
@@ -1,178 +1,178 @@
 module.exports = [
-    /* Octavos */
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  /* Octavos */
+  {
+    team: 'Países Bajos',
+    src: '/img/banderas/holanda.png',
+    alt: 'Bandera de Holanda',
+  },
+  {
+    team: 'Estados Unidos',
+    src: '/img/banderas/estados-unidos.png',
+    alt: 'Bandera de Estados Unidos',
+  },
+  {
+    team: 'Argentina',
+    src: '/img/banderas/argentina.png',
+    alt: 'Bandera de Argentina',
+  },
+  {
+    team: 'Australia',
+    src: '/img/banderas/australia.png',
+    alt: 'Bandera de Australia',
+  },
+  {
+    team: 'Japón',
+    src: '/img/banderas/japon.png',
+    alt: 'Bandera de Japón',
+  },
+  {
+    team: 'Croacia',
+    src: '/img/banderas/croacia.png',
+    alt: 'Bandera de Croacia',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Francia',
+    src: '/img/banderas/francia.png',
+    alt: 'Bandera de Francia',
+  },
+  {
+    team: 'Polonia',
+    src: '/img/banderas/polonia.png',
+    alt: 'Bandera de Polonia',
+  },
+  {
+    team: 'Inglaterra',
+    src: '/img/banderas/inglaterra.png',
+    alt: 'Bandera de Inglaterra',
+  },
+  {
+    team: 'Senegal',
+    src: '/img/banderas/senegal.png',
+    alt: 'Bandera de Senegal',
+  },
+  {
+    team: 'Marruecos',
+    src: '/img/banderas/marruecos.png',
+    alt: 'Bandera de Marruecos',
+  },
+  {
+    team: 'España',
+    src: '/img/banderas/españa.png',
+    alt: 'Bandera de España',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    /* Cuartos */ 
+  /* Cuartos */
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    /* Semis */
+  /* Semis */
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    /* Final */ 
+  /* Final */
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
 
-    /* Tercer puesto */
+  /* Tercer puesto */
 
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    },
-    {
-        team: 'Sin definir',
-        src: '/img/banderas/banderas.png',
-        alt: 'Bandera genérica'
-    }
-]
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+  {
+    team: 'Sin definir',
+    src: '/img/banderas/banderas.png',
+    alt: 'Bandera genérica',
+  },
+];
